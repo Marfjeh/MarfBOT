ce20750655fe2d10d6b36e8550fa25d92f0bd2ef
marfjeh <marfie1997@ziggo.nl>
2017-06-21 14:28:11 +0000

    ! live.js reverted to commit 46fe0df36eb1f1a8fe9f4026ec49a8d72c1f9b59
    Signed-off-by: marfjeh <marfie1997@ziggo.nl>

--- Src/cmd/music/live.js
+++ Src/cmd/music/live.js
@@ -1,31 +1,26 @@
 const   commando = require('discord.js-commando'),
-        marfBOT = require("../../MarfBOT.js"),
-        discord = require("discord.js"),
-        ytdl = require('ytdl-core'),
-        client = new discord.Client();
-
-class LiveCommand extends commando.Command {
-    constructor(client) {
-        super(client, {
-            name: 'live',
-            group: 'music',
-            memberName: 'live',
-            description: 'play a live stream! usage: ]live <url>'
-        });
-    }
-
-    async run(message, args) {
-        if (args.srartsWith("https://www.youtu")) {
-            message.reply("Sorry, Youtube live streams are not supported yet on this MarfBot Version. :(")
-        }
-        else
-        {
-        const streamOptions = { seek: 0, volume: 1 };
-        message.member.voiceChannel.join().then(connection => {
-          connection.playStream(args);
-        }).catch(marfBOT.elog);
-
-    }
-}
-
-module.exports = LiveCommand;
+	        marfBOT = require("../../MarfBOT.js"),
+	        discord = require("discord.js"),
+	        ytdl = require('ytdl-core'),
+	        client = new discord.Client();
+	
+	class LiveCommand extends commando.Command {
+	    constructor(client) {
+	        super(client, {
+	            name: 'live',
+	            group: 'music',
+	            memberName: 'live',
+	            description: 'play a live stream! usage: ]live <url>'
+	        });
+	    }
+	
+	    async run(message, args) {
+	        const streamOptions = { seek: 0, volume: 1 };
+	        message.member.voiceChannel.join().then(connection => {
+	          connection.playStream(args);
+	        }).catch(marfBOT.elog);
+	
+	    }
+	}
+	
+	module.exports = LiveCommand;
