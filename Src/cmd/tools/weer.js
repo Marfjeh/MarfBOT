6dacc9f37165a842390a03ab860ebe108597d18d
Marfjeh <marfie1997@ziggo.nl>
2017-09-12 11:54:45 +0200

    cleaned up weer.js but i'm thinkgin to switch useing cases. but who cares. why i'm i even doing this commit.
    Signed-off-by: Marfjeh <marfie1997@ziggo.nl>

--- Src/cmd/tools/weer.js
+++ Src/cmd/tools/weer.js
@@ -12,61 +12,48 @@ class weerCommand extends commando.Command {
     }
 
     async run(message, args) {
-        let date   = new Date();
-        let n      = date.toDateString();
-        let time   = date.toLocaleTimeString();
-
-	    let titlee = "Buienradar";
-	    let urla   = "http://www.buienradar.nl/";
-	    let mapurl = "https://api.buienradar.nl/image/1.0/RadarMapNL?w=1024&h=1024&random=" + Math.random() + ".gif";
-
-        if (args.toLowerCase() === "europe" || args.toLowerCase() === "eur") {
-			mapurl   = "https://api.buienradar.nl/image/1.0/radarmapeu/?ext=gif&random=" + Math.random() + ".gif";
-			urla     = "https://www.buienradar.nl/wereldwijd/europa/buienradar/3uurs";
-			titlee   = "Buienradar - Europe"
-		}
-		if (args.toLowerCase() === "latvain" || args.toLowerCase() === "lv" || args.toLowerCase() === "latvia" || args.toLowerCase() === "latvija" || args.toLowerCase() === "latveja") {
-			mapurl   = "http://lietus.lv/sri/srilast.gif?random=" + Math.random()+ ".gif";
-			urla     = "http://lietus.lv/";
-			titlee   = "SRI - rain/snow radar";
-		}
-		if (args.toLowerCase() === "cloud" || args.toLowerCase() === "wolken") {
-			mapurl   = "https://api.buienradar.nl/image/1.0/cloudmapnl/?ext=gif&random=" + Math.random() + ".gif";
-			urla     = "https://www.buienradar.nl/nederland/zon-en-wolken/wolkenradar";
-			titlee   = "Buienradar - CloudRadar";
-		}
-        if (args.toLowerCase() === "thunder" || args.toLowerCase() === "onweer") {
-            mapurl  = "https://api.buienradar.nl/image/1.0/lightningnl/?ext=gif&random=" + Math.random() + ".gif";
-            urla    = "https://www.buienradar.nl/nederland/neerslag/onweerradar";
-            titlee  = "Buienradar - Onweer/Thunder";
+        if (args === null || args === "") {
+            sendmap("https://api.buienradar.nl/image/1.0/RadarMapNL?w=1024&h=1024&random=" + Math.random() + ".gif", "http://www.buienradar.nl/", "Buienradar");
         }
-        if (args.toLowerCase() === "uv" || args.toLowerCase() === "sunpower" || args.toLowerCase() === "zonkracht") {
-            mapurl  = "https://api.buienradar.nl/image/1.0/sunpowereu?forc=32&random=" + Math.random() + ".gif";
-            urla    = "https://www.buienradar.nl/nederland/zon-en-wolken/zonkracht-uv";
-            titlee  = "Buienradar - UV Radar";
+        else if (args.toLowerCase() === "europe" || args.toLowerCase() === "eur") {
+            sendmap("https://api.buienradar.nl/image/1.0/radarmapeu/?ext=gif&random=" + Math.random() + ".gif", "https://www.buienradar.nl/wereldwijd/europa/buienradar/3uurs", "Buienradar - Europe");
+		}
+		else if (args.toLowerCase() === "latvain" || args.toLowerCase() === "lv" || args.toLowerCase() === "latvia" || args.toLowerCase() === "latvija" || args.toLowerCase() === "latveja") {
+            sendmap("http://lietus.lv/sri/srilast.gif?random=" + Math.random()+ ".gif", "http://lietus.lv/", "SRI - rain/snow radar");
+		}
+		else if (args.toLowerCase() === "cloud" || args.toLowerCase() === "wolken") {
+            sendmap("https://api.buienradar.nl/image/1.0/cloudmapnl/?ext=gif&random=" + Math.random() + ".gif", "https://www.buienradar.nl/nederland/zon-en-wolken/wolkenradar", "Buienradar - CloudRadar");
+		}
+        else if (args.toLowerCase() === "thunder" || args.toLowerCase() === "onweer") {
+            sendmap("https://api.buienradar.nl/image/1.0/lightningnl/?ext=gif&random=" + Math.random() + ".gif", "https://www.buienradar.nl/nederland/neerslag/onweerradar", "Buienradar - Onweer/Thunder");
         }
-        if (args.toLowerCase() === "usa" || args.toLowerCase() === "north-america" || args.toLowerCase() === "america") {
-            mapurl  = "https://api.buienradar.nl/image/1.0/satvisualworld/?ext=gif&hist=7&forc=1&step=0&type=ge&random=" + Math.random() + ".gif";
-            urla    = "https://www.buienradar.nl/wereldwijd/noord-amerika/satelliet";
-            titlee  = "Buienradar - north-america";
+        else if (args.toLowerCase() === "uv" || args.toLowerCase() === "sunpower" || args.toLowerCase() === "zonkracht") {
+            sendmap("https://api.buienradar.nl/image/1.0/sunpowereu?forc=32&random=" + Math.random() + ".gif", "https://www.buienradar.nl/nederland/zon-en-wolken/zonkracht-uv", "Buienradar - UV Radar");
         }
-        if (args.toLowerCase() === "temp" || args.toLowerCase() === "tempratuur") {
+        else if (args.toLowerCase() === "usa" || args.toLowerCase() === "north-america" || args.toLowerCase() === "america") {
+            sendmap("https://api.buienradar.nl/image/1.0/satvisualworld/?ext=gif&hist=7&forc=1&step=0&type=ge&random=" + Math.random() + ".gif", "https://www.buienradar.nl/wereldwijd/noord-amerika/satelliet", "Buienradar - North-America");
+        }
+        else if (args.toLowerCase() === "temp" || args.toLowerCase() === "tempratuur") {
             //soon (TM)
         }
+        else {
+            message.channel.send(":warning: Invailid argument!");
+        }
 
-        message.channel.send({
-        "embed": {
-                title: titlee,
-                url: urla,
-                "image": {
-                    "url": mapurl,
-                },
-                footer: {
-                    text: n + " " + time,
-                    icon_url: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678120-calendar-clock-512.png'
-                }
-            }
-        });
+        function sendmap(map_image, url_website, message_title) {
+            //This is a work around for discord image caching. If you run the same command a day after it will still show the last image it sended. This will resolve this issue.
+            let map_image_nocache = map_image + "&random=" + Math.random() + ".gif";
+
+            message.channel.send({
+                "embed": {
+                        title: message_title,
+                        url: url_website,
+                        "image": {
+                            "url": map_image_nocache,
+                        }
+                    }
+                });
+        }
     }
 }
 
